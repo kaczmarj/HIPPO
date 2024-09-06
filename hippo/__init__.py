bc031bc75c0ecfd3196107ac25d0eb79e57ed2c1
Jakub Kaczmarzyk <jakub.kaczmarzyk@gmail.com>
2024-09-06 13:45:19 +0200

    add __version__

--- hippo/__init__.py
+++ hippo/__init__.py
@@ -1,4 +1,9 @@
 from hippo.models.abmil import AttentionMILModel
 from hippo.search import greedy_search, maximize, minimize, smallest_difference
 
+try:
+    from hippo._version import __version__
+except ImportError:
+    __version__ = "0+unknown"
+
 __all__ = ["AttentionMILModel", "greedy_search", "maximize", "minimize", "smallest_difference"]
